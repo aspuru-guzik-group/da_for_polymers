f29fcbf8a7007ad35a58d542888c989351c52ed8
stanlo229 <stanley.lo@mail.utoronto.ca>
2023-03-31 17:35:17 -0400

    finished automated fragment, apply to other datasets after assigning *

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
+# Augmenting Polymer Datasets via Iterative Rearrangement
 
+[![DOI](https://zenodo.org/badge/570637902.svg)](https://zenodo.org/badge/latestdoi/570637902)
+
+Welcome to the GitHub repository for the paper: https://doi.org/10.26434/chemrxiv-2022-hxvcc
+
+Abstract: One of the biggest obstacles to successful polymer property prediction is an effective representation that accurately captures the sequence of repeat units in a polymer. Motivated by the successes of data augmentation in computer vision and natural language processing, we explore augmenting polymer data by rearranging the molecular representation while preserving the correct connectivity, revealing additional substructural information that is not present in a single representation. We evaluate the effects of this technique on the performance of machine learning models trained on three experimental polymer datasets and compare them to common molecular representations. Data augmentation improves deep learning property prediction performance compared to equivalent (non-augmented) representations. In datasets where the target property is primarily influenced by the polymer sequence rather than experimental parameters, this data augmentation technique provides the molecular embedding with more information to improve property prediction accuracy.
+
+Keywords: data augmentation, machine learning, polymers, molecular representation
+
+![alt text](https://github.com/stanlo229/da_for_polymers/blob/main/TOC_1.png?raw=true)
+![alt text](https://github.com/stanlo229/da_for_polymers/blob/main/TOC_2.png?raw=true)
+
+
+## Getting Started
+1. Fork, Clone, or Download this repository.
+2. Create a conda environment.
+3. In this directory,  `pip install -e .`
+4. Access raw data  ->  `da_for_polymers/data/raw/Dataset`
+5. Access data augmentation tool ->  `da_for_polymers/data/input_representation/Dataset/manual_frag/augment_manual_frag.py`
+6. Access processed data ->  `da_for_polymers/data/input_representation/Dataset`
+6. Access prediction results -> `da_for_polymers/training`
+7. Run the shell files in `da_for_polymers/ML_models/pytorch/Dataset` or `da_for_polymers/ML_models/sklearn/Dataset` to run models on your setup. To run specific models or molecular representations, uncomment or comment specific lines. Recommened to understand how to use argument parsers for these scripts.
+8. To view or re-create figures, go to -> `da_for_polymers/visualization`. Each figure in the paper can be recreated with the `recreate_all_plots.sh` file (read comments). (Supplementary Figures are found in `da_for_polymers/data/exploration`)g
--- da_for_polymers/data/automated_fragmentation/auto_augment.py
+++ da_for_polymers/data/automated_fragmentation/auto_augment.py
@@ -3,16 +3,44 @@ from pathlib import Path
 from rdkit import Chem
 import pandas as pd
 from rdkit.Chem import Draw
+import copy
+import sys
+from collections import deque
+sys.setrecursionlimit(100)
 
 co2_data: Path = (
     pathlib.Path(__file__).parent.parent / "raw" / "CO2_Soleimani" / "co2_expt_data.csv"
 )
+augmented_co2_data: Path = (
+    pathlib.Path(__file__).parent.parent
+    / "input_representation"
+    / "CO2_Soleimani"
+    / "automated_fragment"
+    / "master_automated_fragment.csv"
+)
+
 pv_data: Path = (
     pathlib.Path(__file__).parent.parent / "raw" / "PV_Wang" / "pv_exptresults.csv"
 )
+augmented_pv_data: Path = (
+    pathlib.Path(__file__).parent.parent
+    / "input_representation"
+    / "PV_Wang"
+    / "automated_fragment"
+    / "master_automated_fragment.csv"
+)
+
 swell_data: Path = (
     pathlib.Path(__file__).parent.parent / "raw" / "Swelling_Xu" / "ps_exptresults.csv"
 )
+augmented_swell_data: Path = (
+    pathlib.Path(__file__).parent.parent
+    / "input_representation"
+    / "Swelling_Xu"
+    / "automated_fragment"
+    / "master_automated_fragment.csv"
+)
+
 dft_data: Path = (
     pathlib.Path(__file__).parent.parent
     / "raw"
@@ -20,8 +48,66 @@ dft_data: Path = (
     / "dft_exptresults.csv"
 )
 
+augmented_dft_data: Path = (
+    pathlib.Path(__file__).parent.parent
+    / "input_representation"
+    / "DFT_Ramprasad"
+    / "automated_fragment"
+    / "master_automated_fragment.csv"
+)
 
-def fragment_polymer(polymer: str) -> Chem.Mol:
+def recursively_find_sidechains(mol:Chem.Mol, sidechains_idx: list, current_idx: int, current_fragment: dict, backbone_idx: list, asterick_idx: list) -> list:
+    """Recursively find the sidechains of a polymer molecule.
+
+    Args:
+        mol: A polymer molecule.
+        sidechains_idx: The sidechain indices of the current sidechain.
+        current_idx: The current index.
+        current_fragment: Dictionary with backbone, sidechain, and connections indices of the fragment of interest.
+        backbone_idx: List of backbone indices of the whole fragment.
+
+    Returns:
+        A polymer molecule with its monomer units fragmented.
+    """
+    # base case
+    # print(f"{sidechains_idx=}")
+    if current_idx in sidechains_idx:
+        return sidechains_idx
+    # recursive case
+    else:
+        atom: Chem.Atom = mol.GetAtomWithIdx(current_idx)
+        # print(f"{atom.GetIdx()=}")
+        # End of sidechain, add current idx to sidechain_idx
+        if len(atom.GetNeighbors()) == 1 and current_idx not in backbone_idx and current_idx not in current_fragment["sidechain"] and current_idx not in current_fragment["backbone"] and current_idx not in asterick_idx:
+            sidechains_idx.append(current_idx)
+            return recursively_find_sidechains(mol, sidechains_idx, current_idx, current_fragment, backbone_idx, asterick_idx)
+        # In the middle of sidechain or in a ring
+        else:
+            # If in ring
+            # print(f"{sidechains_idx=}")
+            if atom.IsInRing():
+                all_atoms_in_ring_added: bool = True
+                for neighbor in atom.GetNeighbors():
+                    if neighbor.GetIdx() not in sidechains_idx:
+                        all_atoms_in_ring_added = False
+                if all_atoms_in_ring_added:
+                    sidechains_idx.append(current_idx)
+                    return sidechains_idx
+                else:
+                    for neighbor in atom.GetNeighbors():
+                        # print(f"{neighbor.GetIdx()=}")
+                        if neighbor.GetIdx() not in sidechains_idx and neighbor.GetIdx() not in backbone_idx and neighbor.GetIdx() not in current_fragment["sidechain"] and neighbor.GetIdx() not in current_fragment["backbone"] and neighbor.GetIdx() not in asterick_idx:
+                            sidechains_idx.append(current_idx)
+                            return recursively_find_sidechains(mol, sidechains_idx, neighbor.GetIdx(), current_fragment, backbone_idx, asterick_idx)
+            # In middle of sidechain
+            else:
+                for neighbor in atom.GetNeighbors():
+                    # print(f"{neighbor.GetIdx()=}")
+                    if neighbor.GetIdx() not in sidechains_idx and neighbor.GetIdx() not in backbone_idx and neighbor.GetIdx() not in current_fragment["sidechain"] and neighbor.GetIdx() not in current_fragment["backbone"] and neighbor.GetIdx() not in asterick_idx:
+                        sidechains_idx.append(current_idx)
+                        return recursively_find_sidechains(mol, sidechains_idx, neighbor.GetIdx(), current_fragment, backbone_idx, asterick_idx)
+
+def get_fragment_indices(polymer: str) -> list[dict]:
     """Fragment a polymer molecule into its monomer units. The order of the monomer units is preserved.
 
     Args:
@@ -31,11 +117,12 @@ def fragment_polymer(polymer: str) -> Chem.Mol:
         A polymer molecule with its monomer units fragmented.
     """
     mol: Chem.Mol = Chem.MolFromSmiles(polymer)
+    ring_info: Chem.RingInfo = mol.GetRingInfo()
     # visualize molecule
     # Set atom index to be displayed
     for atom in mol.GetAtoms():
         atom.SetAtomMapNum(atom.GetIdx())
-    Draw.MolToImageFile(mol, pathlib.Path(__file__).parent / "mol.png", size=(300, 300))
+    # Draw.MolToImageFile(mol, pathlib.Path(__file__).parent / "mol.png", size=(300, 300))
     # find index of astericks
     asterick_idx: list = []
     for atom in mol.GetAtoms():
@@ -49,53 +136,143 @@ def fragment_polymer(polymer: str) -> Chem.Mol:
     asterick_idx_2: int = backbone_idx[-1]
     asterick_idxs: list = [asterick_idx_1, asterick_idx_2]
     backbone_idx: list = backbone_idx[1:-1]
+    # print(f"{backbone_idx=}")
+    # Get all atoms that are non-backbone
+    mol_idx: list = [atom.GetIdx() for atom in mol.GetAtoms()]
+    mol_idx.remove(asterick_idx_1)
+    mol_idx.remove(asterick_idx_2)
     # fragment the smallest building blocks possible
     # If not in ring, add new fragment
     # If in ring, add new fragment if ring is not already in base_fragments
     base_fragments: list = []
     for b_idx in backbone_idx:
+        # print(f"{base_fragments=}")
         atom: Chem.Atom = mol.GetAtomWithIdx(b_idx)
+        present: bool = False
         if atom.IsInRing():
             # check if ring is already in base_fragments
             for i in range(0, len(base_fragments)):
                 if b_idx in base_fragments[i]["backbone"]:
                     current_unit_idx: int = i
-                    base_fragments[i]["backbone"].append(b_idx)
-                else:
-                    base_fragments.append(
-                        {"backbone": [b_idx], "sidechain": [], "connections": []}
-                    )
-                    current_unit_idx = len(base_fragments) - 1
+                    present: bool = True
+            if not present:
+                base_fragments.append(
+                    {"backbone": [b_idx], "sidechain": [], "connections": []}
+                )
+                current_unit_idx = len(base_fragments) - 1
         else:
             base_fragments.append(
                 {"backbone": [b_idx], "sidechain": [], "connections": []}
             )
             current_unit_idx = len(base_fragments) - 1
+
         # check connected atoms
         for neighbor in atom.GetNeighbors():
-            # Ring fragment in backbone
-            if neighbor.IsInRing() and neighbor.GetIdx() in backbone_idx:
-                base_fragments[current_unit_idx]["backbone"].append(neighbor.GetIdx())
-            # Ring fragment not in backbone
-            elif neighbor.IsInRing() and neighbor.GetIdx() not in backbone_idx:
-                base_fragments[current_unit_idx]["backbone"].append(neighbor.GetIdx())
-            # Sidechain of a fragment
-            elif (
-                neighbor.GetIdx() not in backbone_idx
-                and neighbor.GetIdx() not in asterick_idxs
-            ):
-                # fragment
-                base_fragments[current_unit_idx].append(neighbor.GetIdx())
-                break
-    print(f"{backbone_idx=}")
-    mol_idx: list = [atom.GetIdx() for atom in mol.GetAtoms()]
-    mol_idx.remove(asterick_idx_1)
-    mol_idx.remove(asterick_idx_2)
-    print(f"{mol_idx=}")
-    pass
+            # print(f"{neighbor.GetIdx()=}")
+            # Neighboring ring atoms (in the same ring)
+            if atom.IsInRing():
+                # Get ring info for this ring
+                which_ring: tuple = ring_info.AtomMembers(atom.GetIdx())
+                # find all backbone atoms that are connected to the ring
+                for ring_idx in which_ring:
+                    ring_idxs: tuple = ring_info.AtomRings()[ring_idx]
+                    base_fragments[current_unit_idx]["backbone"].extend(ring_idxs)
+                base_fragments[current_unit_idx]["backbone"]: list = list(dict.fromkeys(base_fragments[current_unit_idx]["backbone"]))
+                # find all sidechain atoms connected to backbone atoms in ring
+                for ring_backbone_idx in base_fragments[current_unit_idx]["backbone"]:
+                    ring_backbone_mol: Chem.Mol = mol.GetAtomWithIdx(ring_backbone_idx)
+                    for r_neighbor in ring_backbone_mol.GetNeighbors():
+                        if r_neighbor.GetIdx() not in base_fragments[current_unit_idx]["backbone"] and r_neighbor.GetIdx() not in backbone_idx:
+                            # print(f"{r_neighbor.GetIdx()=}")
+                            # print(f"{recursively_find_sidechains(mol, [], r_neighbor.GetIdx(), base_fragments[current_unit_idx], backbone_idx, asterick_idx)}")
+                            if recursively_find_sidechains(mol, [], r_neighbor.GetIdx(), base_fragments[current_unit_idx], backbone_idx, asterick_idx) is not None:
+                                base_fragments[current_unit_idx]["sidechain"].extend(recursively_find_sidechains(mol, [], r_neighbor.GetIdx(), base_fragments[current_unit_idx], backbone_idx, asterick_idx))
+                # Find connections
+                if neighbor.GetIdx() in backbone_idx and neighbor.GetIdx() not in base_fragments[current_unit_idx]["backbone"]:
+                    base_fragments[current_unit_idx]["connections"].append(neighbor.GetIdx())
+            else:
+                if (
+                    neighbor.GetIdx() not in backbone_idx
+                    and neighbor.GetIdx() not in asterick_idxs
+                ):
+                    base_fragments[current_unit_idx]["sidechain"].append(neighbor.GetIdx())
+                # New fragment
+                elif neighbor.GetIdx() in backbone_idx and neighbor.GetIdx() not in base_fragments[current_unit_idx]["backbone"]:
+                    base_fragments[current_unit_idx]["connections"].append(neighbor.GetIdx())
+                
+    return mol, base_fragments
 
+def fragment_mol_from_indices(mol: Chem.Mol, base_fragments: list[dict]) -> list[Chem.Mol]:
+    """Fragment a polymer molecule into its monomer units.
 
-def iterative_shuffle(fragmented: Chem.Mol) -> list:
+    Args:
+        mol: A polymer molecule.
+        base_fragments: A list of dictionaries with backbone, sidechain, and connections indices of the fragments of interest.
+
+    Returns:
+        A list of polymer molecules with its monomer units fragmented.
+    """
+    if len(base_fragments) == 1:
+        [a.SetAtomMapNum(0) for a in mol.GetAtoms()]
+        mol_smi: str = Chem.MolToSmiles(mol)
+        mol_smi: str = mol_smi.replace("*", "")
+        return [mol_smi]
+    # print(Chem.MolToSmiles(mol))
+    fragments: list[Chem.Mol] = []
+    # Fragment on connection bonds
+    # print(f"{base_fragments=}" )
+    bonds_to_fragment: list[int] = []
+    for i in range(0, len(base_fragments)-1):
+        fragment: dict = base_fragments[i]
+        next_fragment: dict = base_fragments[i+1]
+        for connection in fragment["connections"]:
+            if connection in next_fragment["backbone"]:
+                atom_1: int = connection
+        for next_connection in next_fragment["connections"]:
+            if next_connection in fragment["backbone"]:
+                atom_2: int = next_connection
+        # print(f"{base_fragments[i]=}")
+        # print(f"{base_fragments[i+1]=}")
+        # print(f"{mol.GetBondBetweenAtoms(atom_1, atom_2)=}")
+        bonds_to_fragment.append(mol.GetBondBetweenAtoms(atom_1, atom_2).GetIdx())
+    new_mol: Chem.Mol = Chem.FragmentOnBonds(mol, bonds_to_fragment, addDummies=False)
+    new_mol_smi: str = Chem.MolToSmiles(new_mol)
+    new_frags: list[str] = new_mol_smi.split(".") # split on "." to get fragments
+    # Re-order fragments based on base_fragments connections
+    ordered_frags: list[str] = []
+    # print(f"{new_frags=}")
+    for i in range(0, len(base_fragments)):
+        for frag in new_frags:
+            matched = True
+            # all idx in backbone must be matched
+            for b_idx in base_fragments[i]["backbone"]:
+                str_check: str = ":" + str(b_idx) + "]"
+                if str_check not in frag:
+                    matched = False
+            if matched:
+                ordered_frags.append(frag)
+    # print(f"{ordered_frags=}")
+    # Remove astericks on fragments
+    ordered_fragments_without_astericks: list[str] = []
+    for i in range(0, len(ordered_frags)):
+        mol_frag: Chem.Mol = Chem.MolFromSmiles(ordered_frags[i])
+        # Remove atom mapping
+        [a.SetAtomMapNum(0) for a in mol_frag.GetAtoms()]
+        # remove dummy atom
+        mol_frag_smi: str = Chem.MolToSmiles(mol_frag)
+        mol_frag_smi: str = mol_frag_smi.replace("*", "")
+        # edmol_frag: Chem.EditableMol = Chem.EditableMol(mol_frag)
+        # for atom in mol_frag.GetAtoms():
+        #     if atom.GetSymbol() == "*":
+        #         edmol_frag.RemoveAtom(atom.GetIdx())
+        # mol_frag = edmol_frag.GetMol()
+        # mol_frag = Chem.rdmolops.RemoveHs(mol_frag, implicitOnly=False)
+        # Draw.MolToImageFile(mol_frag, pathlib.Path(__file__).parent / "mol_frag.png", size=(300, 300))
+        ordered_fragments_without_astericks.append(mol_frag_smi)
+
+    return ordered_fragments_without_astericks
+
+def iterative_shuffle(fragmented: list[str]) -> list[list[str]]:
     """Iteratively shuffle the fragments of a polymer molecule.
 
     Args:
@@ -104,10 +281,18 @@ def iterative_shuffle(fragmented: Chem.Mol) -> list:
     Returns:
         A polymer molecule with its monomer units shuffled. (list[list[str]])
     """
-    pass
+    augmented_polymer_list = []
+    polymer_frag_deque = deque(copy.copy(fragmented))
+    for j in range(len(fragmented)):
+        frag_rotate = copy.copy(polymer_frag_deque)
+        frag_rotate.rotate(j)
+        frag_rotate = list(frag_rotate)
+        augmented_polymer_list.append(frag_rotate)
+    
+    return augmented_polymer_list
 
 
-def augment_dataset(dataset: Path) -> pd.DataFrame:
+def augment_dataset(dataset: Path, augmented_dataset: Path) -> pd.DataFrame:
     """Augment the dataset by iteratively shuffling the fragments of a polymer.
 
     Args:
@@ -117,9 +302,13 @@ def augment_dataset(dataset: Path) -> pd.DataFrame:
         A dataset with the experimental data.
     """
     dataset_df: pd.DataFrame = pd.read_csv(dataset)
-    dataset_df["augmented_polymer"] = dataset_df["smiles"].map(
-        lambda x: iterative_shuffle(fragment_polymer(x))
-    )
+    indices: tuple[Chem.Mol, dict] = dataset_df.apply(lambda x: get_fragment_indices(x["smiles"]), axis=1)
+    fragment_mols = indices.apply(lambda x: fragment_mol_from_indices(x[0], x[1]))
+    shuffled = fragment_mols.apply(lambda x: iterative_shuffle(x))
+    dataset_df["augmented_polymer"] = shuffled
+
+    dataset_df.to_csv(augmented_dataset, index=False)
+    
 
     pass
 
@@ -137,5 +326,11 @@ def filter_dataset(dataset: Path, property: str) -> pd.DataFrame:
     pass
 
 
-# augment_dataset(dft_data)
-fragment_polymer("NC2C(O)C(OC1OC(CO)C(O(C))C(O[*])C1N)C(CO)OC2([*])")
+# augment_dataset(dft_data, augmented_dft_data)
+# get_fragment_indices("*c1ccc(-c2nc3cc4nc(*)oc4cc3o2)cc1")
+# get_fragment_indices("NC2C(O)C(OC1OC(COCCC)C(O(C))C(O[*])C1N)C(CO)OC2([*])")
+# mol, base_fragments = get_fragment_indices("NC2C(O)C(OC1OC(COC3CCCC3)C(O(C))C(O[*])C1N)C(CO)OC2([*])")
+# fragmented = fragment_mol_from_indices(mol, base_fragments)
+# shuffled = iterative_shuffle(fragmented)
+# print(Chem.MolFromSmiles("*c1ccc(-c2nc3cc4nc(*)oc4cc3o2)cc1"))
+# print(augmented_dft_data)
