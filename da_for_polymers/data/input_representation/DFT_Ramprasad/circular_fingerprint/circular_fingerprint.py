02fe2c292f5e436b7660ad794042a8c9ddafa4d4
Stanley Lo <stanley.lo@mail.utoronto.ca>
2023-04-17 16:06:52 -0400

    finished circular fingerprint

--- da_for_polymers/data/input_representation/DFT_Ramprasad/circular_fingerprint/circular_fingerprint.py
+++ da_for_polymers/data/input_representation/DFT_Ramprasad/circular_fingerprint/circular_fingerprint.py
@@ -16,6 +16,8 @@ CFP_DFT_RAMPRASAD = pkg_resources.resource_filename(
     "data/input_representation/DFT_Ramprasad/circular_fingerprint/dft_circular_fingerprint_Egc.csv",
 )
 
+current_dir = Path(__file__).resolve().parent
+
 
 # create a function that produces dimer, trimers, and polymer graph (RDKiT, add a new bond at the ends of the trimer)
 def Nmer_graph(expt_results_path: str, cfp_path: str, N: int) -> pd.DataFrame:
@@ -34,7 +36,7 @@ def Nmer_graph(expt_results_path: str, cfp_path: str, N: int) -> pd.DataFrame:
         monomer_smi: str = row["smiles"]
         monomer_mol: Chem.rdchem.Mol = Chem.MolFromSmiles(monomer_smi)
         curr_mol: Chem.rdchem.Mol = copy.deepcopy(monomer_mol)
-        for i in range(N):
+        for i in range(N - 1):
             curr_mol: Chem.rdchem.Mol = Chem.CombineMols(curr_mol, monomer_mol)
             # Get index of last atom of first monomer by atom index and atomic number
             dummy_atom_idx: list = []
@@ -67,10 +69,84 @@ def Nmer_graph(expt_results_path: str, cfp_path: str, N: int) -> pd.DataFrame:
     expt_results.to_csv(cfp_path, index=False)
 
 
-def polymer_graph(expt_results_path: str, cfp_path: str) -> pd.DataFrame:
-    pass
+def polymer_graph(expt_results_path: str, cfp_path: str, N: int) -> pd.DataFrame:
+    """Create a dataframe of polymer graphs.
+
+    Args:
+        expt_results_path (str): _description_
+        cfp_path (str): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    expt_results: pd.DataFrame = pd.read_csv(expt_results_path)
+    expt_results[f"{N}mer_circular_graph_fp"] = ""
+    for index, row in expt_results.iterrows():
+        monomer_smi: str = row["smiles"]
+        monomer_mol: Chem.rdchem.Mol = Chem.MolFromSmiles(monomer_smi)
+        curr_mol: Chem.rdchem.Mol = copy.deepcopy(monomer_mol)
+        for i in range(N - 1):
+            curr_mol: Chem.rdchem.Mol = Chem.CombineMols(curr_mol, monomer_mol)
+            # Get index of last atom of first monomer by atom index and atomic number
+            dummy_atom_idx: list = []
+            dummy_atom_neighbor_idx: list = []
+            for atom in curr_mol.GetAtoms():
+                if atom.GetAtomicNum() == 0 and len(atom.GetNeighbors()) == 1:
+                    dummy_atom_idx.append(atom.GetIdx())
+                    dummy_atom_neighbor_idx.append(atom.GetNeighbors()[0].GetIdx())
+            # make into editable mol
+            ed_mol: Chem.rdchem.EditableMol = Chem.EditableMol(curr_mol)
+            # add bond between last atom of first monomer and first atom of second monomer
+            ed_mol.BeginBatchEdit()
+            ed_mol.AddBond(
+                dummy_atom_neighbor_idx[1],
+                dummy_atom_neighbor_idx[2],
+                Chem.rdchem.BondType.SINGLE,
+            )
+            # delete dummy atom around the new bond
+            ed_mol.RemoveAtom(dummy_atom_idx[1])
+            ed_mol.RemoveAtom(dummy_atom_idx[2])
+            ed_mol.CommitBatchEdit()
+            # convert back to mol
+            curr_mol: Chem.rdchem.Mol = ed_mol.GetMol()
+        # NEW: add bond from first dummy atom to last dummy atom
+        dummy_atom_idx: list = []
+        dummy_atom_neighbor_idx: list = []
+        for atom in curr_mol.GetAtoms():
+            if atom.GetAtomicNum() == 0 and len(atom.GetNeighbors()) == 1:
+                dummy_atom_idx.append(atom.GetIdx())
+                dummy_atom_neighbor_idx.append(atom.GetNeighbors()[0].GetIdx())
+        # print(dummy_atom_idx)
+        # Draw.MolToFile(
+        #     curr_mol,
+        #     filename=current_dir / "monomer.png",
+        #     size=(500, 500),
+        # )
+        # make into editable mol
+        ed_mol: Chem.rdchem.EditableMol = Chem.EditableMol(curr_mol)
+        # add bond between last atom of first monomer and first atom of second monomer
+        ed_mol.BeginBatchEdit()
+        ed_mol.AddBond(
+            dummy_atom_neighbor_idx[0],
+            dummy_atom_neighbor_idx[-1],
+            Chem.rdchem.BondType.SINGLE,
+        )
+        # delete dummy atom around the new bond
+        ed_mol.RemoveAtom(dummy_atom_idx[0])
+        ed_mol.RemoveAtom(dummy_atom_idx[-1])
+        ed_mol.CommitBatchEdit()
+        # convert back to mol
+        curr_mol: Chem.rdchem.Mol = ed_mol.GetMol()
+
+        # convert to fingerprint
+        Chem.SanitizeMol(curr_mol)
+        curr_mol_fp: np.ndarray = np.array(
+            AllChem.GetMorganFingerprintAsBitVect(curr_mol, 3, nBits=512)
+        )
+        expt_results.at[index, f"{N}mer_circular_graph_fp"] = curr_mol_fp
+    expt_results.to_csv(cfp_path, index=False)
 
 
 if __name__ == "__main__":
     # Nmer_graph(CFP_DFT_RAMPRASAD, CFP_DFT_RAMPRASAD, 3)
-    polymer_graph(CFP_DFT_RAMPRASAD, CFP_DFT_RAMPRASAD)
+    polymer_graph(CFP_DFT_RAMPRASAD, CFP_DFT_RAMPRASAD, 1)
