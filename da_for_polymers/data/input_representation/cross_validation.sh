675390f02493cac9858e95ed9f38b0bb7fb81745
stanlo229 <stanley.lo@mail.utoronto.ca>
2023-04-26 16:30:49 -0400

    todo: re-run models on polymer stratified K-Folds

--- da_for_polymers/data/input_representation/cross_validation.sh
+++ da_for_polymers/data/input_representation/cross_validation.sh
@@ -2,13 +2,29 @@
 
 # python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/DFT_Ramprasad/augmentation/train_aug_master.csv --num_of_folds 5 --type_of_crossval KFold
 
-python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/DFT_Ramprasad/automated_fragment/master_automated_fragment_Egc.csv --num_of_folds 5 --type_of_crossval KFold
+# python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/DFT_Ramprasad/automated_fragment/master_automated_fragment_Egc.csv --num_of_folds 5 --type_of_crossval KFold
 
 # python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/DFT_Ramprasad/SMILES/dft_smiles.csv --num_of_folds 5 --type_of_crossval KFold
 
 # python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/SMILES/master_smiles.csv --num_of_folds 6 --type_of_crossval StratifiedKFold --stratified_label Solvent
 
-# python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/Swelling_Xu/SMILES/master_smiles.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Solvent
+python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/Swelling_Xu/manual_frag/master_manual_frag.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+# python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/Swelling_Xu/augmentation/train_aug_master.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+# python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/Swelling_Xu/automated_fragment/master_automated_fragment.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+# python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/Swelling_Xu/BRICS/master_brics_frag.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+# python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/Swelling_Xu/circular_fingerprint/ps_circular_fingerprint.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+# python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/Swelling_Xu/fingerprint/ps_fingerprint.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+# python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/Swelling_Xu/ohe/master_ohe.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+# python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/Swelling_Xu/SMILES/master_smiles.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+
+python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/augmentation/train_aug_master.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/automated_fragment/master_automated_fragment.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/BRICS/master_brics_frag.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/circular_fingerprint/pv_circular_fingerprint.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/fingerprint/pv_fingerprint.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/ohe/master_ohe.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/SMILES/master_smiles.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
+python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/manual_frag/master_manual_frag.csv --num_of_folds 7 --type_of_crossval StratifiedKFold --stratified_label Polymer
 
 # python ./cross_validation.py --dataset_path ~/Research/Repos/da_for_polymers/da_for_polymers/data/input_representation/PV_Wang/fingerprint/pv_fingerprint.csv --num_of_folds 6 --type_of_crossval StratifiedKFold --stratified_label Solvent
 
